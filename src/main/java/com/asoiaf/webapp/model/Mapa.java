ec536c431dd9c339820de7bd4338d38f88c0c12d
Patricia Escurin <patricia.sanz.escurin@gmail.com>
2022-10-23 10:28:00 +0200

    Modelo de mapas creados

---
+++ src/main/java/com/asoiaf/webapp/model/Mapa.java
@@ -0,0 +1,44 @@
+package com.asoiaf.webapp.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+public class Mapa implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String nombre;
+    private String imagen;
+
+    public Mapa() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+}
--- src/main/java/com/asoiaf/webapp/model/Usuario.java
+++ src/main/java/com/asoiaf/webapp/model/Usuario.java
@@ -1,2 +1,62 @@
-package com.asoiaf.webapp.model;public class Usuario {
+package com.asoiaf.webapp.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String email;
+    private String nombre;
+    private String contraseña;
+    private String casaElegida;
+
+    public Usuario() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getCasaElegida() {
+        return casaElegida;
+    }
+
+    public void setCasaElegida(String casaElegida) {
+        this.casaElegida = casaElegida;
+    }
 }
